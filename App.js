89f7bc6f7f54798e3318c8d4731b035c4da6cf0e
clintfidel <clintfidel@gmail.com>
2019-07-18 23:21:29 +0000

    get users location

--- App.js
+++ App.js
@@ -1,22 +1,64 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import FetchLocation from './components/FetchLocation';
 import UsersMap from './components/UsersMap';
 
 
-const handleGetLocation = () =>  {
-  navigator.geolocation.getCurrentPosition(position => {
-    console.log(position)
-  }, error => {
-    throw error
-  });
-}
+
 export default function App() {
+  const [userLocation, setUserLocation] = useState(null);
+  const [usersPlaces, setUsersPlace] = useState([]);
+  const handleGetLocation = () =>  {
+    navigator.geolocation.getCurrentPosition(position => {
+      setUserLocation({
+        ...userLocation, 
+        latitude: parseFloat(position.coords.latitude),
+        longitude: parseFloat(position.coords.longitude),
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,     
+      });
+      fetch('https://airopay-1.firebaseio.com/places.json', {
+        method: 'POST',
+        body: JSON.stringify({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        })
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+    }, (error) => alert(JSON.stringify(error))),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+  }
+
+  const handleGetPlaces = () => {
+    fetch('https://airopay-1.firebaseio.com/places.json', {
+        method: 'GET',
+      })
+      .then(res => res.json())
+      .then(response => {
+        const placesArray = [];
+        for(const key in response) {
+          placesArray.push({
+            latitude: response[key].latitude,
+            longitude: response[key].longitude,
+            id: key
+          })
+        }
+        setUsersPlace(placesArray)
+      })
+      .catch(err => console.log(err))
+  }
   return (
     <View style={styles.container}>
+      <View style = {styles.placesButton}>
+        <Button title = 'Get User places' onPress={handleGetPlaces}/>
+      </View>
       <FetchLocation 
         locationData = {handleGetLocation} />
-      <UsersMap />
+      <UsersMap 
+        usersLocation = {userLocation} 
+        usersPlaces = {usersPlaces}
+      />
     </View>
   );
 }
@@ -24,8 +66,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffff',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  placesButton: {
+    marginBottom: 20
   }
 });
--- components/UsersMap.js
+++ components/UsersMap.js
@@ -1,11 +1,33 @@
 import react from 'react';
 import { View, StyleSheet} from 'react-native';
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 
-const UsersMap = ({}) => {
+const UsersMap = ({ usersLocation, usersPlaces }) => {
+	let usersLocationMarker = null;
+	if(usersLocation) {
+		usersLocationMarker = (<Marker coordinate = {usersLocation}/>)
+	}
+	const usersPlacesMaker = usersPlaces.map(userPlaces => 
+		<MapView.Marker 
+			coordinate = {userPlaces} 
+			key={userPlaces.id}/>)
   return (
 		<View style = {styles.mapContainer}>
-			<MapView style = {styles.map} />
+			<MapView 
+				 initialRegion={{
+					latitude: parseFloat(37.78825),
+					longitude: parseFloat(-122.4324),
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421,
+    		}}
+				region={usersLocation}
+				style = {styles.map} 
+				showsUserLocation = {true}
+				>
+				{usersLocationMarker}
+				{usersPlacesMaker}
+
+				</MapView>
 		</View>
 	)
 }
@@ -13,8 +35,10 @@ const UsersMap = ({}) => {
 const styles = StyleSheet.create({
 	mapContainer : {
 		width: '100%',
-		height: 200,
-		marginTop: 20
+		height: 400,
+		marginTop: 20,
+	
+		
 	},
 	map: {
 		width: '100%',
